22ad1996fe7cd90e6ee8e67e4f4fee3c89b36393
Mohamed Fariz <faris872010@gmail.com>
2021-06-14 18:25:44 +0530

    React js Tailwind Css Starter - Inital Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-900 text-green-300 flex-col">
+
+      <h1 className="text-4xl font-semibold block mb-4">React Tailwind CSS Starter</h1>
+      <a href="https://github.com/farisnceit" target="_blank" className="text-center" rel="noreferrer">
+      <img src="https://avatars.githubusercontent.com/u/17798473?v=4" className="rounded-full w-64 mb-6" alt="M Mohamed Fariz" />
+      <p className="font-medium text-3xl">M Mohamed Fariz </p>
+      <p>faris872010@gmail.com </p></a>
+     
+    </div>
+  );
+}
+
+export default App;
